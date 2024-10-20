97a9ce091f1f6bcd294983408a4f5f5c7b4dc888
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 11:22:27 +0530

    Models Added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-const PORT = 4003;
+app.get('/', (req, res) => {
+	  res.send('Hello World!');
+});
 
-app.listen(PORT, (req, res) => {
-	res.send('server started')
-}
+app.listen(PORT, () => {
+	  console.log(`Example app listening at http://localhost:${PORT}`);
+});
